3eb80538133114cf8a722493c32e9a48fc783a47
Volodya Meda <volodyameda@MacBook-Pro-Volodya.local>
2020-03-31 17:54:08 +0300

    Initial commit

---
+++ examples/api_usage_example.py
@@ -0,0 +1,21 @@
+from ebaysdk.finding import Connection as Finding
+from ebaysdk.exception import ConnectionError
+
+
+def ebayapiexample():
+    '''
+    Example of using an ebay API
+    Returns information about one found product placed on ebay
+    ('Stephen King The Shining' - as an example)
+    ebay.json - api configuration
+    '''
+    try:
+        api = Finding(config_file='ebay.json', appid='Volodymy-Expendit-PRD-3196809ca-9c1b019a')
+
+        request = {'keywords': 'Stephen King The Shining'}
+        response = api.execute('findCompletedItems', request)
+
+        return response.dict()['searchResult']['item'][0]
+
+    except ConnectionError as err:
+        return err.response.dict()
