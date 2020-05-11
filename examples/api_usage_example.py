6b19f8195b969f5a71c7f4d1ae13ee7c1a9ed780
Volodya Meda <volodyameda@MacBook-Pro-Volodya.local>
2020-05-11 21:08:43 +0300

    Initial commit

--- examples/api_usage_example.py
+++ examples/api_usage_example.py
@@ -1,7 +1,6 @@
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 
-
 def ebayapiexample():
     '''
     Example of using an ebay API
@@ -10,12 +9,12 @@ def ebayapiexample():
     ebay.json - api configuration
     '''
     try:
-        api = Finding(config_file='ebay.json', appid='Volodymy-Expendit-PRD-3196809ca-9c1b019a')
+        api = Finding(config_file='ebay.json', appid='*')
 
         request = {'keywords': 'Stephen King The Shining'}
         response = api.execute('findItemsAdvanced', request)
 
-        return response.dict()['searchResult']['item'][0]
+        return response.dict()['searchResult']['item'][0]['sellingStatus']
 
     except ConnectionError as err:
         return err.response.dict()
